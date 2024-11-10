01ac3661f5340a7ed768260e706523850037cbae
Shah Jabir <shahjabirtaqi@gmail.com>
2024-11-10 13:25:31 +0600

    feature: backend and reddis database added

---
+++ src/app/api/[[...route]]/route.ts
@@ -0,0 +1,57 @@
+import { Redis } from '@upstash/redis';
+import { Hono } from 'hono';
+import { env } from 'hono/adapter';
+import { handle } from 'hono/vercel';
+
+export const runtime = 'edge';
+
+const app = new Hono().basePath('/api');
+
+type EnvConfig = {
+  UPTASH_REDIS_REST_TOKEN: string;
+  UPTASH_REDIS_REST_URL: string;
+};
+
+app.get('/search', async (c) => {
+  try {
+    const { UPTASH_REDIS_REST_TOKEN, UPTASH_REDIS_REST_URL } =
+      env<EnvConfig>(c);
+    const start = performance.now();
+    // ---------------------------------
+    const redis = new Redis({
+      token: UPTASH_REDIS_REST_TOKEN,
+      url: UPTASH_REDIS_REST_URL,
+    });
+
+    const query = c.req.query('q')?.toUpperCase();
+
+    if (!query) {
+      return c.json({ message: 'Invalid search query' }, { status: 400 });
+    }
+    const res = [];
+    const rank = await redis.zrank('terms', query);
+    if (rank !== null && rank !== undefined) {
+      const temp = await redis.zrange<string[]>('terms', rank, rank + 150);
+      for (const el of temp) {
+        if (!el.startsWith(query)) {
+          break;
+        }
+        if (el.endsWith('*')) {
+          res.push(el.substring(0, el.length - 1));
+        }
+      }
+    }
+    // -----------------------------------
+    const end = performance.now();
+    return c.json({
+      results: res,
+      duration: end - start,
+    });
+  } catch (error) {
+    console.error(error);
+    return c.json({ results: [], message: error }, { status: 500 });
+  }
+});
+
+export const GET = handle(app);
+export default app as never;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,38 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+  const [input, setInput] = useState<string>();
+  const [searchResults, setSearchResults] = useState<{
+    results: string[];
+    duration: number;
+  }>();
+
+  useEffect(() => {
+    const fecthData = async () => {
+      if (!input) {
+        setSearchResults(undefined);
+      }
+      const res = await fetch(`/api/search?q=${input}`);
+      const data = await res.json();
+      console.log(data);
+    };
+    fecthData();
+  }, [input]);
+
+  console.log(searchResults);
+
   return (
     <>
       <div>
         <h1>Fast API</h1>
+        <input
+          type="text"
+          className="text-zinc-900"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
       </div>
     </>
   );
